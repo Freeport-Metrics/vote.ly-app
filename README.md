3fb447e9bf5060cdf3911bb6af97041ab52debba
Daniel Piltch <dan@freeportmetrics.com>
2013-10-18 15:42:28 -0400

    Dynamic question appears

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ vote.ly-app
 ===========
 
 The app part of the world's best mobile voting app.
+
+Current Issues
+==============
+
+* Works in Firefox but not in Safari or Chrome.  Switching API to port 80 seems to fix this.
+* Might need to refresh voting page to see question ... issue with pageinit/pageshow being fired
--- app.js
+++ app.js
@@ -1,8 +1,17 @@
   function VotingAPI(){}
 
-  VotingAPI.Questions = function Votings(){}
+  var baseURL = 'http://83.144.67.188:6666/VotingApiService.svc';
 
-  VotingAPI.Questions.get = function(userId, callback) {
-    requestURL = 'http://83.144.67.188:6666/VotingApiService.svc/GetVotings?userId=' + userId + '&callback=?';
+  VotingAPI.Questions = function Questions(){}
+  VotingAPI.Answers = function Questions(){}
+
+  VotingAPI.Questions.get = function(votingId, callback) {
+    requestURL = baseURL + '/GetQuestions?votingId=' + votingId + '&callback=?';
     $.getJSON(requestURL, callback);
-  }
+  };
+
+  VotingAPI.Answers.get = function(questionId, callback) {
+    requestURL = baseURL + '/GetAnswears?questionId=' + questionId + '&callback=?';
+    $.getJSON(requestURL, callback);
+  };
+
